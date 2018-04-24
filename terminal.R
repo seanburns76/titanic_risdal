87e0bf163642a62f6fb5bc2fd7b380ce55fe2259
Sean Burns <mrsean.burns@icloud.com>
2018-04-24 15:49:05 -0500

    commit at line 36..first plot

--- terminal.R
+++ terminal.R
@@ -13,3 +13,28 @@ test <- read.csv('C:/Users/SeaBur/Documents/GitProjects/titanic_risdal/test.csv'
 full <- bind_rows(train,test)
 str(full)
 head(full)
+
+full$Title <- gsub('(.*, )|(\\..*)', '',full$Name)
+table(full$Sex,full$Title)
+
+rare_title <- c('Dona','Lady','the Countess','Capt','Col','Don','Dr','Major','Rev','Sir','Jonkheer')
+
+full$Title[full$Title == 'Mlle'] <- 'Miss'
+full$Title[full$Title == 'Ms'] <- 'Miss'
+full$Title[full$Title == 'Mme'] <- 'Mrs'
+full$Title[full$Title %in% rare_title] <- 'Rare Title'
+
+table(full$Sex,full$Title)
+help("sapply")
+help("fun")
+full$Surname <- sapply(full$Name,function(x) strsplit(x,split = '[,.]')[[1]][1])
+full$Surname
+full$Fsize <- full$SibSp + full$Parch + 1
+full$Family <- paste(full$Surname,full$Fsize,sep = '_')
+
+
+ggplot(full[1:891,],aes(x=Fsize,fill = factor(Survived))) + 
+    geom_bar(stat = 'count',position = 'dodge') + 
+      scale_x_continuous(breaks = c(1:11)) + 
+        labs(x='Family Size') +
+          theme_few()
