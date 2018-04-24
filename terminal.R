56520339647d242ddbf4c25ade303bed139da4b0
seanburns76 <mrsean.burns@icloud.com>
2018-04-24 00:20:23 -0500

    first few lines libraries,read in data

---
+++ terminal.R
@@ -0,0 +1,14 @@
+
+
+library(ggplot2)
+library(ggthemes)
+library(scales)
+library(dplyr)
+library(randomForest)
+library(mice)
+
+train <- read.csv('C:/Users/SeaBur/Documents/GitProjects/titanic_risdal/train.csv',stringsAsFactors = F)
+test <- read.csv('C:/Users/SeaBur/Documents/GitProjects/titanic_risdal/test.csv',stringsAsFactors = F)
+
+full <- bind_rows(train,test)
+str(full)
